0345e274581489bbabb0607b66e731a8b46ce1e4
Michael Testori <business.michaeltestori@gmail.com>
2024-07-06 18:09:55 +0200

    a simple program in C#

---
+++ firstProgram.cs
@@ -0,0 +1,20 @@
+// basic I/O C# library
+using System;
+
+class Program
+{ 
+    // main() method
+    static void Main()
+    {
+        // Console.Write VS Console.WriteLine
+        Console.WriteLine("|#|==========================|#|==========================|#|");
+        Console.WriteLine(" ");
+        Console.Write("Congratulations!");
+        Console.Write(" ");
+        Console.WriteLine("You wrote your first lines of code in C#.");
+        Console.WriteLine("Coded By Michael Testori | username: Powerbliz00");
+        Console.WriteLine(" ");
+        Console.WriteLine("|#|==========================|#|==========================|#|");
+        
+    }
+}
